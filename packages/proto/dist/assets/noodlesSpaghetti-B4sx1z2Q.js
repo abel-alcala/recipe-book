f2fbb30955609cae552799f579d82e01d6c43537
abel-alcala <alcalaabel4@gmail.com>
2025-05-07 23:11:16 -0700

    added dist

---
+++ packages/proto/dist/assets/noodlesSpaghetti-B4sx1z2Q.js
@@ -0,0 +1,1 @@
+import{d as n,I as e}from"./IngredientCardJson-C_W9pUNE.js";n({"ingredient-info":e});
---
+++ packages/proto/vite.config.js
@@ -0,0 +1,25 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                chefAbel: resolve(__dirname, 'Chef-Abel.html'),
+                cuisineItalian: resolve(__dirname, 'cuisine-italian.html'),
+                garlic: resolve(__dirname, 'garlic.html'),
+                groundBeef: resolve(__dirname, 'ground-beef.html'),
+                mealplan: resolve(__dirname, 'mealplan.html'),
+                noodlesSpaghetti: resolve(__dirname, 'noodles-spaghetti.html'),
+                oliveOil: resolve(__dirname, 'olive-oil.html'),
+                onion: resolve(__dirname, 'onion.html'),
+                recipeSpaghetti: resolve(__dirname, 'Recipe-spaghetti.html'),
+                tomato: resolve(__dirname, 'tomato.html')
+            },
+        },
+    },
+})
